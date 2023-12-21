657845e4b1c292404044113129128986c13f664f
sweetmjkim <sweet.mjkim@gmail.com>
2023-12-21 17:48:43 +0900

    first commit

---
+++ docs/formats.py
@@ -0,0 +1,24 @@
+# * 웹 크롤링 동작
+from selenium import webdriver
+
+# ChromeDriver 실행
+browser = webdriver.Chrome("../ChromeDriver.exe")
+
+# Chrome WebDriver의 capabilities 속성 사용
+capabilities = browser.capabilities
+
+# - 주소에 https://www.w3schools.com/ 입력
+browser.get("https://www.w3schools.com/")                   # 크롬 브라우저에 주소 넣는거와 같음.
+
+# - 가능 여부에 대한 ok 사인
+pass
+# - html 파일 받음(and 확인)
+html = browser.page_source
+print(html)
+
+# - 정보 획득
+pass
+# 브라우저 종료
+browser.quit()
+
+
---
+++ docs/installchromedrivers.py
@@ -0,0 +1,7 @@
+# - chrome browser 열기
+from webdriver_manager.chrome import ChromeDriverManager
+
+# ChromeDriver 업데이트
+webdriver_manager = ChromeDriverManager().install()
+
+pass
