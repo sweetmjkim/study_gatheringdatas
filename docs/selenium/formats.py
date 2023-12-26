016433caa622717faa78490f163cd87aaa253f4e
sweetmjkim <sweet.mjkim@gmail.com>
2023-12-26 09:46:57 +0900

    selenium

--- docs/selenium/formats.py
+++ docs/selenium/formats.py
@@ -1,11 +1,13 @@
 # * 웹 크롤링 동작
+# (install browser)
+# setup driver
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
 webdriver_manager_directory = ChromeDriverManager().install()
 
-# ChromeDriver 실행
+# ChromeDriver 실행(Chrome 열기)
 browser = webdriver.Chrome(service=ChromeService(webdriver_manager_directory))
 
 # Chrome WebDriver의 capabilities 속성 사용
@@ -14,9 +16,9 @@ capabilities = browser.capabilities
 # - 주소 입력(https://www.w3schools.com/) 후 Enter
 browser.get("https://www.w3schools.com/")                          # 크롬 브라우저에 주소 넣는거와 같음.
 
-# - 가능 여부에 대한 OK 받음
+# - 가능 여부에 대한 OK 받음(selenium으로 대신한다.)
 pass
-# - html 파일 받음(and 확인)
+# - html 파일 받음(and 확인)(selenium으로 대신한다.)
 html = browser.page_source
 # print(html)
 
