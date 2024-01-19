4ec5c189eb7eb2a8341c800c2252fe76f7a0e17e
sweetmjkim <sweet.mjkim@gmail.com>
2024-01-19 18:18:23 +0900

    mj

--- docs/quests/APIs/PubDataOpnStdService.py
+++ docs/quests/APIs/PubDataOpnStdService.py
@@ -1,8 +1,8 @@
 import requests
 
-url = 'http://apis.data.go.kr/1230000/PubDataOpnStdService/getDataSetOpnStdBidPblancInfo'
+url = 'http://apis.data.go.kr/1230000/PubDataOpnStdService/getDataSetOpnStdBidPblancInfo'                           # OpenAPI를 가지고 오기위한 url주소
 
-params = {'serviceKey' : 'GiqSMasfSR7XrTX/5uIxIdKTvXYAXTPdcKpnBtPNDH9yBKpc9Hcv2+mzhTo6ra260JiX3a0IS1A5QDTBgrRoRw=='
+params = {'serviceKey' : 'GiqSMasfSR7XrTX/5uIxIdKTvXYAXTPdcKpnBtPNDH9yBKpc9Hcv2+mzhTo6ra260JiX3a0IS1A5QDTBgrRoRw==' # OpenAPI의 각각의 정보들
           ,'pageNo' : 1
           ,'numOfRows' : 10
           ,'type' : 'json'
@@ -17,10 +17,9 @@ import json
 contents = json.loads(response.content)
 pass
 
-# mongodb 저장
-from pymongo import MongoClient
+from pymongo import MongoClient                                     # mongodb 저장
 mongoClient = MongoClient("mongodb://localhost:27017")              # mongodb에 접속 -> 자원에 대한 class
 database = mongoClient["data_go_kr"]                                # database 연결
-collection = database['PubDataOpnStdService']              # collection 작업
-result = collection.insert_many(contents['response']['body']['items'])
+collection = database['PubDataOpnStdService']                       # collection 작업
+result = collection.insert_many(contents['response']['body']['items'])      # insert 작업 진행
 pass
---
+++ docs/quests/APIs/search_news_navercom shop.py
@@ -0,0 +1,41 @@
+# from : https://developers.naver.com/docs/serviceapi/search/shopping/shopping.md#%EC%87%BC%ED%95%91
+
+import requests         #postman app 역할
+
+# request API 요청
+url = 'https://openapi.naver.com/v1/search/shop'
+params = {'query' : '블랙박스'}
+headers = {'X-Naver-Client-Id' : 'JpVPHqeIjR0_FhfaGShS'
+           ,'X-Naver-Client-Secret' : 'rWFwXYlaa5'}
+
+response = requests.get(url, params=params, headers=headers)
+
+# response API 응답
+response.content
+
+# json을 변수로 변환
+import json
+contents = json.loads(response.content)
+
+from pymongo import MongoClient
+mongoClient = MongoClient("mongodb://localhost:27017")              # mongodb에 접속 -> 자원에 대한 class
+database = mongoClient["data_go_kr"]                                # database 연결
+collection = database['search_shop_info']                           # collection 작업
+collection1 = database['search_shop_list']                           # collection 작업
+pass
+
+info = {
+    "lastBuildDate" : contents['lastBuildDate']
+    ,"total" : contents['total']
+    ,"start" : contents['start']
+    ,"display" : contents['display']
+    }
+result_info = collection.insert_one(info)      # insert 작업 진행
+pass
+
+id_relative = result_info.inserted_id
+
+for i in contents['items'] : 
+    i['id_relative'] = id_relative
+collection1.insert_many(contents['items'])
+pass
